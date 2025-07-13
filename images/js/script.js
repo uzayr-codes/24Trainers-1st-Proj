cc0b4a1f502b712593b8720608c4bf8d7344d47a
uzayr-codes <pirachauzair7@gmail.com>
2025-07-13 23:53:42 +0500

    AutoSlide

--- images/js/script.js
+++ images/js/script.js
@@ -1,6 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 3000, 
+    disableOnInteraction: false, 
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
